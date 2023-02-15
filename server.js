92dedb1485cad8fff6f26da70a7e081c4459b2b4
Lao <estanislaolanus@gmail.com>
2023-02-15 10:32:34 -0300

    Upgrade

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-
-    res.send("Hello world with railway")
+    const { name } = req.query;
+    res.send(`Hello ${name}! We are using railway`)
 });
+
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => {
     console.log(`RUN http://localhost:${PORT}`)
